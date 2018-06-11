7b20e4c7be04923c094770ba79090c71353dedef
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-11 17:56:29 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,42 @@
 class ProductsController < ApplicationController
+  before_action :set_product, :except => [:index, :new, :create]
+  
   def index
     @products = Product.all
   end
-
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
-
+  
   def new
     @product = Product.new
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @product = Product.new(product_params)
+    if @product.save then redirect_to products_path
+    else render :new and return
+    end
+  end
+  
+  def show
+  end
+  
+  def data
+    binding.pry
+    render :json => ProductSerializer.serialize(@product)
+  end
+
+  def description
+    render :plain => @product.description
+  end
+
+  def inventory
+    render :plain => !!( @product.inventory > 0 )
   end
 
   private
+  
+  def set_product
+    @product = Product.find_by(:id => params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
